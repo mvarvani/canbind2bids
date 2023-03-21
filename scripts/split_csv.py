e814cec3c49110aac640d4d741d13a90bbb1dd00
Tristan Kuehn <tkuehn@uwo.ca>
2023-03-21 14:04:00 -0400

    Update manual example TSV and JSON

--- scripts/split_csv.py
+++ scripts/split_csv.py
@@ -6,6 +6,6 @@ df = pd.read_table(snakemake.input.csv, sep=',',dtype={'subjects':str})
 tsvs = np.array_split(df, df.shape[0])
 
 for i, tsv in enumerate(tsvs):
-    tsv.to_csv(snakemake.output.tsv.format(i), sep='\t', index=False, header=False)
+    tsv.to_csv(snakemake.output.tsv.format(i), sep='\t', index=False, header=True)
 
 #how to make (i) the subject ID?
