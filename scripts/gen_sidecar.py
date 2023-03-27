e5deb5e24844401a34aca1e7e2a11b752708b4b5
Tristan Kuehn <tristankuehn@gmail.com>
2023-03-27 11:21:28 -0400

    Fix gen_sidecar script

--- scripts/gen_sidecar.py
+++ scripts/gen_sidecar.py
@@ -1,14 +1,18 @@
+"""Handle conversion of CANBIND documentation to BIDS"""
+from __future__ import annotations
+
+import json
+from typing import Any, Optional
+
 import pandas as pd
 
 
-def reformat_table(table: pd.DataFrame):
-    """Reformat a table to conform to the BIDS standard."""
-    return table.assign(
-        participant_id=table.SUBJLABEL.str.split("_").str[1:3].str.join("")
-    ).drop(columns=["SUBJLABEL"])
-
-
 def gen_description(row: pd.Series) -> str:
+    """Generate a column description.
+
+    The generated description will come from question text and special
+    instructions as applicable.
+    """
     question_text = (
         f"Question text: {row['Question text']}"
         if pd.notna(row["Question text"])
@@ -28,15 +32,32 @@ def gen_description(row: pd.Series) -> str:
     )
 
 
-def handle_levels(row: pd.Series):
+def handle_levels(row: pd.Series) -> Optional[dict[str, str]]:
+    """Generate a Levels object for a row if applicable."""
     if row["Variable Type"] not in ["single-select", "radio"]:
         return None
-    return dict([item.split(" = ") for item in row["Value Codes"].split("; ")])
+
+    for delimiter in [";", ","]:
+        try:
+            return {
+                key.strip(): val.strip()
+                for key, val in [
+                    item.strip().split("=")
+                    for item in row["Value Codes"].split(delimiter)
+                ]
+            }
+        except ValueError:
+            # The delimiter was probably wrong
+            pass
+    raise ValueError("Could not parse row with any delimiter.")
 
 
-def row_to_dict(row: pd.Series):
+def row_to_dict(row: pd.Series) -> dict[str, Any]:
+    """Generate a BIDS TSV sidecar-formatted dict from a row."""
     return (
-        row["Variable Name"],
+        row["Variable Name"]
+        if row["Variable Name"] != "SUBJLABEL"
+        else "participant_id",
         dict(
             [("LongName", row["Item Name"]), ("Description", gen_description(row))]
             + ([("Levels", levels)] if (levels := handle_levels(row)) else [])
@@ -44,9 +65,12 @@ def row_to_dict(row: pd.Series):
     )
 
 
-def main(doc_path, out_path):
+def main(doc_path: str, out_path: str) -> None:
+    """Read a CANBIND excel documentation file and reformat it to BIDS JSON."""
     table = pd.read_excel(doc_path)
+    with open(out_path, "w", encoding="utf-8") as out_file:
+        json.dump(dict(list(table.apply(row_to_dict, axis=1).values)), out_file)
 
 
 if __name__ == "__main__":
-    main(snakemake.input.control_csv, snakemake.input.patient_csv, snakemake.output.tsv)
+    main(snakemake.input.doc_excel, snakemake.output.json)
